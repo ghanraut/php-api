bda7042f95b184103b0cf21b762dcf367b8dd6e2
ghanraut <narayanrupraut@gmail.com>
2015-11-19 18:38:00 +0530

    Updated domain,api key
    Updated domain,api key and email with sample data

--- curlwrap_v2.php
+++ curlwrap_v2.php
@@ -1,8 +1,8 @@
 <?php
 # Enter your domain name , agile email and agile api key
-define("AGILE_DOMAIN", "ghanshyam");  # Example : define("domain","jim");
-define("AGILE_USER_EMAIL", "ghanshyam.raut@agilecrm.com"); 
-define("AGILE_REST_API_KEY", "123456"); // Example : http://snag.gy/AEq23.jpg
+define("AGILE_DOMAIN", "domain");  # Example : define("domain","jim");
+define("AGILE_USER_EMAIL", "user_email"); 
+define("AGILE_REST_API_KEY", "*******"); // Example : http://snag.gy/AEq23.jpg
 function curl_wrap($entity, $data, $method, $content_type)
 {
     if($content_type==NULL){
